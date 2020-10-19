80eeba5240a0fb6613c4dffad494451cc0754fa5
zstevens-aicradle <zstevens@safex.ai>
2020-10-19 14:44:01 -0600

    Separate main and neptune files.

---
+++ coda/neptune.py
@@ -0,0 +1,46 @@
+from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
+from gremlin_python.structure.graph import Graph
+from gremlin_python.process.traversal import T
+
+graph = Graph()
+remoteConn = DriverRemoteConnection('wss://sensing-tf3.cluster-cfzqaib9mklx.us-west-2.neptune.amazonaws.com:8182/gremlin','g')
+
+def getPersons():
+    g = graph.traversal().withRemote(remoteConn)
+    nodes = g.V().hasLabel('dummy_person').valueMap(True).toList()
+    return {x[T.id]: {'personId': x['person_id'][0], 'name': x['name'][0], 'income': x['income'][0]} for x in nodes}
+
+def getKnows(vertexId):
+    g = graph.traversal().withRemote(remoteConn)
+    return g.V(vertexId).out('knows').id().toList()
+
+def getCommunity(vertexId):
+    g = graph.traversal().withRemote(remoteConn)
+    return g.V(vertexId).out('member_of').id().toList()[0]
+
+def getMappedNodes():
+    # get all individual person nodes
+    mappedNodes = getPersons()
+    for nodeId in mappedNodes:
+        knowsIds = getKnows(nodeId)
+        mappedNodes[nodeId]['knowsIds'] = knowsIds
+        mappedNodes[nodeId]['communityId'] = getCommunity(nodeId)
+    remoteConn.close()
+    return mappedNodes
+
+def getAdjacencyMatrix(nodes):
+    nodeList = [x for x in nodes]
+    matrix = []
+    for x in range(len(nodes)):
+        matrix.append([0 for x in range(len(nodes))])
+    for nodeId in nodes:
+        selfIndex = nodeList.index(nodeId)
+        for knowsId in nodes[nodeId].get('knowsIds', []):
+            knowsIndex = nodeList.index(knowsId)
+            matrix[selfIndex][knowsIndex] = 1
+    return matrix
+
+
+
+
+
