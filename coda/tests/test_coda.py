ae70567396f53d224ab5a8d8716b3dfdebf1b404
zstevens-aicradle <zstevens@safex.ai>
2020-10-19 15:25:41 -0600

    Removed main in favor of tests

--- coda/tests/test_coda.py
+++ coda/tests/test_coda.py
@@ -4,9 +4,11 @@ import pytest
 import numpy
 import pandas
 from ..coda import CODA
+from ..neptune import getMappedNodes, getAdjacencyMatrix
+
 
 def test_coda_on_paper_example():
-	
+
 	S = pandas.DataFrame({'income': [70, 160, 140, 100, 110, 40, 10, 30, 10, 30]})
 	W = numpy.array([[0, 0, 1, 0, 0, 0, 0, 0, 0, 0], # 1 -> 3
 					 [0, 0, 1, 0, 0, 0, 0, 0, 1, 0], # 2 -> 3, 9
@@ -21,6 +23,22 @@ def test_coda_on_paper_example():
 
 	model = CODA(S, W, K=2, lambda_=0.5, n_outliers=1, generating_distribution='independent',
 		return_all=True)
+
+	node, energy = model.run()
+
+	print("node: ", node)
+	print("energy: ", energy)
+	print("sum of energy: ", sum(energy))
+
+	print("made it here")
+
+def test_coda_on_dummy_data():
+	nodes = getMappedNodes()
+	matrix = getAdjacencyMatrix(nodes)
+	S = pandas.DataFrame({'income': [nodes[x]['income'] for x in nodes]})
+	W = numpy.array(matrix)
+	model = CODA(S, W, K=2, lambda_=0.5, n_outliers=1, generating_distribution='independent',
+				 return_all=True)
 
 	node, energy = model.run()
 
